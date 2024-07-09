e38ce82e106e7f2e19dad0c6d34b605b69201db8
zero-mover <zero-mover@users.noreply.github.com>
2024-07-09 02:07:39 +0000

    deploy: 8e132c73f9e48b23c0bcf73fdb4b368b84d57771

---
+++ sui_bridge_indexer/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["config","eth_worker","latest_eth_syncer","metrics","models","postgres_manager","schema","sui_checkpoint_ingestion","sui_transaction_handler","sui_transaction_queries","sui_worker","types"],"struct":["TokenTransfer","TokenTransferData"]};
